0a1c90228193ecc9b34d90f9e43cdb3e9cff0cc4
Warm Beer <mickvd99@gmail.com>
2024-12-20 11:09:53 +0100

    chore: allow tailwind to override primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,77 +1,80 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { definePreset } from "@primevue/themes";
+import {definePreset} from "@primevue/themes";
 import Aura from "@primevue/themes/aura";
 
 const AutonomiPreset = definePreset(Aura, {
-  // Customizations
-  // semantic: {
-  // button: {
-  //   border: {
-  //     radius: "10rem",
-  //   },
-  // },
-  // },
-  semantic: {
-    button: {
-      background: {
-        color: "black",
-      },
+    // Customizations
+    // semantic: {
+    // button: {
+    //   border: {
+    //     radius: "10rem",
+    //   },
+    // },
+    // },
+    semantic: {
+        button: {
+            background: {
+                color: "black",
+            },
+        },
     },
-  },
-  colorScheme: {
-    light: {
-      primary: {
-        color: "#000000",
-      },
+    colorScheme: {
+        light: {
+            primary: {
+                color: "#000000",
+            },
+        },
+        dark: {
+            primary: {
+                color: "#FF0000",
+            },
+        },
     },
-    dark: {
-      primary: {
-        color: "#FF0000",
-      },
-    },
-  },
 });
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  // (optional) Enable the Nuxt devtools
-  devtools: { enabled: true },
-  // Enable SSG
-  ssr: false,
-  // Enables the development server to be discoverable by other devices when running on iOS physical devices
-  devServer: { host: process.env.TAURI_DEV_HOST || "localhost" },
-  vite: {
-    // Better support for Tauri CLI output
-    clearScreen: false,
-    // Enable environment variables
-    // Additional environment variables can be found at
-    // https://v2.tauri.app/reference/environment-variables/
-    envPrefix: ["VITE_", "TAURI_"],
-    server: {
-      // Tauri requires a consistent port
-      strictPort: true,
-    },
-  },
-  css: ["~/assets/css/main.css"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  modules: ["@primevue/nuxt-module", "@pinia/nuxt"],
-  primevue: {
-    options: {
-      ripple: true,
-      inputVariant: "filled",
-      theme: {
-        preset: AutonomiPreset,
-        options: {
-          prefix: "p",
-          darkModeSelector: "autonomi-dark",
-          cssLayer: false,
+    compatibilityDate: "2024-11-01",
+    // (optional) Enable the Nuxt devtools
+    devtools: {enabled: true},
+    // Enable SSG
+    ssr: false,
+    // Enables the development server to be discoverable by other devices when running on iOS physical devices
+    devServer: {host: process.env.TAURI_DEV_HOST || "localhost"},
+    vite: {
+        // Better support for Tauri CLI output
+        clearScreen: false,
+        // Enable environment variables
+        // Additional environment variables can be found at
+        // https://v2.tauri.app/reference/environment-variables/
+        envPrefix: ["VITE_", "TAURI_"],
+        server: {
+            // Tauri requires a consistent port
+            strictPort: true,
         },
-      },
     },
-  },
+    css: ["~/assets/css/main.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    modules: ["@primevue/nuxt-module", "@pinia/nuxt"],
+    primevue: {
+        options: {
+            ripple: true,
+            inputVariant: "filled",
+            theme: {
+                preset: AutonomiPreset,
+                options: {
+                    prefix: "p",
+                    darkModeSelector: "autonomi-dark",
+                    cssLayer: {
+                        name: "primevue",
+                        order: "tailwind-base, primevue, tailwind-utilities"
+                    },
+                },
+            },
+        },
+    },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,61 +1,61 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 const autonomiColors = {
-  red: {
-    300: "#FF3355",
-  },
-  blue: {
-    200: "#A9A9F2",
-    600: "#26264C",
-    800: "#11113B",
-  },
-  gray: {
-    50: "#F8F8FA",
-    100: "#F2F2F6",
-    200: "#E4E7EC",
-    300: "#CCCCCC",
-    500: "#8994A3",
-    600: "#666666",
-  },
-  green: {
-    300: "#60C281",
-  },
+    red: {
+        300: "#FF3355",
+    },
+    blue: {
+        200: "#A9A9F2",
+        600: "#26264C",
+        800: "#11113B",
+    },
+    gray: {
+        50: "#F8F8FA",
+        100: "#F2F2F6",
+        200: "#E4E7EC",
+        300: "#CCCCCC",
+        500: "#8994A3",
+        600: "#666666",
+    },
+    green: {
+        300: "#60C281",
+    },
 };
 
 export default {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./error.vue",
-  ],
-  theme: {
-    colors: {
-      ...colors,
-      autonomi: {
-        ...autonomiColors,
-        header: {
-          text: {
-            DEFAULT: autonomiColors.blue[600],
-            dark: autonomiColors.blue[800],
-          },
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
+    theme: {
+        colors: {
+            ...colors,
+            autonomi: {
+                ...autonomiColors,
+                header: {
+                    text: {
+                        DEFAULT: autonomiColors.blue[600],
+                        dark: autonomiColors.blue[800],
+                    },
+                },
+                text: {
+                    primary: {
+                        DEFAULT: autonomiColors.gray[500],
+                        dark: autonomiColors.gray[300],
+                    },
+                    secondary: {
+                        DEFAULT: autonomiColors.blue[600],
+                        dark: autonomiColors.gray[300],
+                    },
+                },
+            },
         },
-        text: {
-          primary: {
-            DEFAULT: autonomiColors.gray[500],
-            dark: autonomiColors.gray[300],
-          },
-          secondary: {
-            DEFAULT: autonomiColors.blue[600],
-            dark: autonomiColors.gray[300],
-          },
-        },
-      },
+        extend: {},
     },
-    extend: {},
-  },
-  darkMode: ["selector", ".autonomi-dark"],
-  plugins: [],
+    darkMode: ["selector", ".autonomi-dark"],
+    plugins: [require("tailwindcss-primeui")]
 };
